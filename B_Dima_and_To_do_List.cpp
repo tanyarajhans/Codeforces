25299298d70116d6f6ee7286ae8e59609c45dbce
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-01-15 01:47:45 +0530

    more codes

--- B_Dima_and_To_do_List.cpp
+++ B_Dima_and_To_do_List.cpp
@@ -3,7 +3,6 @@
 #ifndef _GLIBCXX_NO_ASSERT
 #include <cassert>
 #endif
-#include <vector>
 #include <cctype>
 #include <cerrno>
 #include <cfloat>
@@ -122,6 +121,7 @@
 #include <version>
 #endif
 using namespace std;
+typedef long long ll;
 
 
 // Tanya Rajhans
@@ -132,19 +132,19 @@ int gcd(int a, int b)
     return gcd(b, a % b); 
      
 }
-   long long pf[10000001];
+   ll pf[10000001];
    void prime(){
        memset(pf,0,10000001);
        pf[0]=pf[1]=1;
-   for(long long i=2;i<10000001;i++){
+   for(ll i=2;i<10000001;i++){
         if(pf[i]==0){
-            for(long long j=i*i;j<10000001;j=j+i)
+            for(ll j=i*i;j<10000001;j=j+i)
                   pf[j]=1;
         }
     }
 }
 
-int check_ps(long long n){
+int check_ps(ll n){
     double sqrt_n = sqrt(n);
     if(sqrt_n == int(sqrt_n)){
         return 1;
@@ -156,38 +156,37 @@ int check_ps(long long n){
 int main(){
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
-int t;
-cin>>t;
-while(t--){
-    int n,k;
-    cin>>n>>k;
-     vector<long long>a(n);
-     vector<long long>b(n);
-      long long mini=1e9;
-      int id=-1;
-       for(int i=0;i<n;i++){
-        cin>>a[i];
-        long long x=a[i];
-         b[i]=0;
-         while(x%k==0){
-          x/=k;
-          b[i]++;
-           }
-          if(b[i]<mini){
-             mini=b[i];
-              id=i;      
-             }
-         }
-           long long ans=0;
-        for(int i=0;i<n;i++){
-          ans+=(mini+1LL)*a[i];
-         }
-        for(int i=0;i<id;i++){
-        ans+=a[i];
-         }
-                    
-    cout<<ans<<endl;
-
+int n,k;
+cin>>n>>k;
+int a[n];
+for(int i=0;i<n;i++){
+    cin>>a[i];
 }
+
+int l=0,j=0,currs=0,maxs=0,ans=0;
+int f=n/k;
+int c[k]={0};
+
+while(l<k){
+currs=0;
+int ind=j;
+while(currs<f){
+c[l]+=a[ind];
+ind+=k;
+currs++;
+}
+j++;
+l++;
+}
+
+int minimum=INT_MAX, mini;
+for(int i=0;i<k;i++){
+    if(minimum>c[i]){
+        minimum=c[i];
+        mini=i;
+    }
+}
+cout<<(mini+1)<<endl;
+
 return 0;
 }
---
+++ B_Painting_Eggs.cpp
@@ -0,0 +1,176 @@
+
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cuchar>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#endif
+
+#if __cplusplus >= 201703L
+#include <any>
+#include <charconv>
+// #include <execution>
+#include <filesystem>
+#include <optional>
+#include <memory_resource>
+#include <string_view>
+#include <variant>
+#endif
+
+#if __cplusplus > 201703L
+#include <bit>
+#include <compare>
+#include <concepts>
+#if __cpp_impl_coroutine
+# include <coroutine>
+#endif
+#include <latch>
+#include <numbers>
+#include <ranges>
+#include <span>
+#include <stop_token>
+#include <semaphore>
+#include <syncstream>
+#include <version>
+#endif
+using namespace std;
+typedef long long ll;
+
+
+// Tanya Rajhans
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b); 
+     
+}
+   ll pf[10000001];
+   void prime(){
+       memset(pf,0,10000001);
+       pf[0]=pf[1]=1;
+   for(ll i=2;i<10000001;i++){
+        if(pf[i]==0){
+            for(ll j=i*i;j<10000001;j=j+i)
+                  pf[j]=1;
+        }
+    }
+}
+
+int check_ps(ll n){
+    double sqrt_n = sqrt(n);
+    if(sqrt_n == int(sqrt_n)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+ll n;
+cin>>n;
+ll p=0,q=0;
+while(n--){
+    int x,y;
+    cin>>x>>y;
+    p+=x;
+    if(p<=500 ){
+      
+      cout<<"A";
+    }
+    else{
+        p-=1000;
+     cout<<"G"; 
+    }
+}
+return 0;
+}
