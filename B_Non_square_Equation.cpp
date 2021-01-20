2ae72dad82961bf192b722563cfa031418aac6ae
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-01-20 21:09:03 +0530

    more codes

---
+++ B_Non_square_Equation.cpp
@@ -0,0 +1,187 @@
+
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cuchar>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#endif
+
+#if __cplusplus >= 201703L
+#include <any>
+#include <charconv>
+// #include <execution>
+#include <filesystem>
+#include <optional>
+#include <memory_resource>
+#include <string_view>
+#include <variant>
+#endif
+
+#if __cplusplus > 201703L
+#include <bit>
+#include <compare>
+#include <concepts>
+#if __cpp_impl_coroutine
+# include <coroutine>
+#endif
+#include <latch>
+#include <numbers>
+#include <ranges>
+#include <span>
+#include <stop_token>
+#include <semaphore>
+#include <syncstream>
+#include <version>
+#endif
+using namespace std;
+typedef long long ll;
+
+
+// Tanya Rajhans
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b); 
+     
+}
+   ll pf[10000001];
+   void prime(){
+       memset(pf,0,10000001);
+       pf[0]=pf[1]=1;
+   for(ll i=2;i<10000001;i++){
+        if(pf[i]==0){
+            for(ll j=i*i;j<10000001;j=j+i)
+                  pf[j]=1;
+        }
+    }
+}
+
+int check_ps(ll n){
+    double sqrt_n = sqrt(n);
+    if(sqrt_n == int(sqrt_n)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int s(int x){
+    int sum=0;
+    while(x>0){
+        sum+=x%10;
+        x/=10;
+    }
+    return sum;
+}
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+ll n;
+cin>>n;
+bool f=false;
+int ans=-1;
+for(int i=1;i<=81;i++){
+   double x=(-i+sqrt(i*i+4*n))/2;
+   double y=(-i-sqrt(i*i+4*n))/2;
+   int p=(int)x;
+   int q=(int)y;
+    if(p==x && q==y && x>0 && s(x)==i){
+        ans=x;
+        break;
+    }
+    if(p==x && q==y && y>0 && s(y)==i){
+        ans=y;
+        break;
+    }
+}
+cout<<ans<<endl;
+return 0;
+}
---
+++ C_Array_Destruction.cpp
@@ -0,0 +1,212 @@
+
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cuchar>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <climits>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#endif
+
+#if __cplusplus >= 201703L
+#include <any>
+#include <charconv>
+// #include <execution>
+#include <filesystem>
+#include <optional>
+#include <memory_resource>
+#include <string_view>
+#include <variant>
+#endif
+
+#if __cplusplus > 201703L
+#include <bit>
+#include <compare>
+#include <concepts>
+#if __cpp_impl_coroutine
+# include <coroutine>
+#endif
+#include <latch>
+#include <numbers>
+#include <ranges>
+#include <span>
+#include <stop_token>
+#include <semaphore>
+#include <syncstream>
+#include <version>
+#endif
+using namespace std;
+typedef long long ll;
+
+
+// Tanya Rajhans
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b); 
+     
+}
+   ll pf[10000001];
+   void prime(){
+       memset(pf,0,10000001);
+       pf[0]=pf[1]=1;
+   for(ll i=2;i<10000001;i++){
+        if(pf[i]==0){
+            for(ll j=i*i;j<10000001;j=j+i)
+                  pf[j]=1;
+        }
+    }
+}
+
+int check_ps(ll n){
+    double sqrt_n = sqrt(n);
+    if(sqrt_n == int(sqrt_n)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    n*=2;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    sort(a,a+n);
+    int maxi=a[n-1];
+    bool f=false;
+    for(int i=0;i<n-1;i++){
+      ll p=a[i]+maxi;
+       ll q=p;
+       map<ll,ll> m;
+       for(int j=0;j<n;j++){
+           m[a[j]]++;
+       }
+       ll c=0;
+       vector< pair < ll, ll> > v;
+       for(ll j=n-1;j>=0;j--){
+           if(m[a[j]]==0)
+           continue;
+           if(m[a[j]]!=0){
+               ll t=q-a[j];
+               m[a[j]]--;
+               if(m[t]>0){
+                   v.push_back(make_pair(t,a[j]));
+                   q=(t>a[j])?t:a[j];
+                   m[t]--;
+                   c+=2;
+               }
+               else break;
+           }
+   
+       }
+       if(c==n){
+           cout<<"YES"<<endl;
+           cout<<p<<endl;
+           for(int k=0;k<v.size();k++){
+               cout<<v[k].first<<" "<<v[k].second<<endl;
+           }
+           f=true;
+           break;
+       }
+    }
+    if(!f){
+       cout<<"NO"<<endl; 
+    }
+}
+return 0;
+}
