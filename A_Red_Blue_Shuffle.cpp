56b7bbe2cf1c5e60e9fecd1f3e6e4065fbbf1a2b
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-01-30 01:09:14 +0530

    more codes

---
+++ A_Red_Blue_Shuffle.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<list>
+#include<set>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<bitset>
+#include<string>
+#include<unordered_set>
+#include<unordered_map>
+#include<cstdlib>
+#include<iomanip>
+#define f               first
+#define s              second
+#define ll               long long
+#define pb               push_back
+#define mp               make_pair
+#define pii              pair<int,int>
+#define vi               vector<int>
+#define mii              unordered_map<int,int>
+#define pqb              priority_queue<int>
+#define pqs              priority_queue< int,vi,greater<int> >
+#define setbits(x)       __builtin_popcountll(x)
+#define zrobits(x)       __builtin_ctzll(x)
+#define mod              1000000007
+#define inf              1e10
+#define ps(x,y)          fixed<<setprecision(y)<<x
+#define mk(arr,n,type)   type *arr=new type[n];
+#define w(x)             int x; cin>>x; while(x--)
+#define me(i,start,end)  for(int i=start;i<end;i++)
+#define he(i,end,start) for(int i=end-1;i>=start;i--)
+#define all(v)         v.begin(),v.end()
+
+using namespace std;
+ll pf[10000001];
+
+
+// Tanya Rajhans
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b); 
+     
+}
+
+   void prime(){
+       memset(pf,0,10000001);
+       pf[0]=pf[1]=1;
+   for(ll i=2;i<10000001;i++){
+        if(pf[i]==0){
+            for(ll j=i*i;j<10000001;j=j+i)
+                  pf[j]=1;
+        }
+    }
+}
+
+int lcm(int a, int b) { return a * b / gcd(a, b); }
+
+int check_ps(ll n){
+    double sqrt_n = sqrt(n);
+    if(sqrt_n == int(sqrt_n)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    string s1;
+    cin>>s1;
+    string s2;
+    cin>>s2;
+    int x=0,y=0;
+    vector< pair< char, char> > v(n);
+    for(int i=0;i<n;i++){
+     if(s1[i]>s2[i])
+     x++;
+     else if(s2[i]>s1[i])
+     y++;
+    }
+    
+    /*do{
+        string p="",q="";
+        for(int i=0;i<n;i++){
+            p+=v[i].f;
+            q+=v[i].s;
+        }
+        if(p.compare(q)>0)
+        x++;
+        else if(q.compare(p)>0)
+        y++;
+    }
+    while(next_permutation(v.begin(),v.end()));*/
+    if(x>y)
+    cout<<"RED";
+    else if(x<y)
+    cout<<"BLUE";
+    else
+    cout<<"EQUAL";
+    cout<<endl;
+}
+return 0;
+}
---
+++ B_New_Year_s_Number.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<list>
+#include<set>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<bitset>
+#include<string>
+#include<unordered_set>
+#include<unordered_map>
+#include<cstdlib>
+#include<iomanip>
+#define f               first
+#define s              second
+#define ll               long long
+#define pb               push_back
+#define mp               make_pair
+#define pii              pair<int,int>
+#define vi               vector<int>
+#define mii              unordered_map<int,int>
+#define pqb              priority_queue<int>
+#define pqs              priority_queue< int,vi,greater<int> >
+#define setbits(x)       __builtin_popcountll(x)
+#define zrobits(x)       __builtin_ctzll(x)
+#define mod              1000000007
+#define inf              1e10
+#define ps(x,y)          fixed<<setprecision(y)<<x
+#define mk(arr,n,type)   type *arr=new type[n];
+#define w(x)             int x; cin>>x; while(x--)
+#define me(i,start,end)  for(int i=start;i<end;i++)
+#define he(i,end,start) for(int i=end-1;i>=start;i--)
+#define all(v)         v.begin(),v.end()
+
+using namespace std;
+ll pf[10000001];
+
+
+// Tanya Rajhans
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b); 
+     
+}
+
+   void prime(){
+       memset(pf,0,10000001);
+       pf[0]=pf[1]=1;
+   for(ll i=2;i<10000001;i++){
+        if(pf[i]==0){
+            for(ll j=i*i;j<10000001;j=j+i)
+                  pf[j]=1;
+        }
+    }
+}
+
+int lcm(int a, int b) { return a * b / gcd(a, b); }
+
+int check_ps(ll n){
+    double sqrt_n = sqrt(n);
+    if(sqrt_n == int(sqrt_n)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+while(t--){
+ll n;
+cin>>n;
+if(n<2020)
+cout<<"NO";
+else{
+ int r=n%2020;
+if((n-r*2021)>=0 && (n-r*2021)%2020==0)
+ cout<<"YES";
+ else
+ cout<<"NO";
+    
+}
+cout<<endl;
+}
+return 0;
+}
